147a4ddad04ef65165490879b22ca6bf9406f205
AKASH2082001 <akashbme208@gmail.com>
2022-03-29 22:30:01 -0700

    Pytest project

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,25 +1,21 @@
 import Calculator
 
-def test_Add():
-    x=10
-    y=2
-    result= Calculator.Add(x,y)
-    assert result == x+y
+import pytest
 
-def test_Sub():
-    x=10
-    y=2
-    result= Calculator.Sub(x,y)
-    assert result == x-y
+@pytest.mark.parametrize("a,b",[(3,2),(5,1),(8,2)])
+def test_Add(a,b):
+    result= Calculator.Add(a,b)
+    assert result == a+b
+@pytest.mark.parametrize("a,b",[(3,8),(5,4),(8,6)])
+def test_Sub(a,b):
 
-def test_Mul():
-    x=10
-    y=2
-    result= Calculator.Mul(x,y)
-    assert result == x*y
-
-def test_Div():
-    x=10
-    y=2
-    result= Calculator.Div(x,y)
-    assert result == x/y
+    result= Calculator.Sub(a,b)
+    assert result == a-b
+@pytest.mark.parametrize("a,b",[(3,5),(5,7),(8,7)])
+def test_Mul(a,b):
+    result= Calculator.Mul(a,b)
+    assert result == a*b
+@pytest.mark.parametrize("a,b",[(3,4),(5,9),(8,3)])
+def test_Div(a,b):
+    result= Calculator.Div(a,b)
+    assert result == a/b
